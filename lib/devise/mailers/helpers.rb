8148c6e209b35861b72a290e502fdca3030835e9
Patrick Morgan <patrick@patrick-morgan.net>
2020-09-09 00:18:50 -0700

    Use layout if possible

--- lib/devise/mailers/helpers.rb
+++ lib/devise/mailers/helpers.rb
@@ -7,64 +7,18 @@ module Devise
 
       included do
         include Devise::Controllers::ScopedViews
-      end
 
-      protected
-
-      attr_reader :scope_name, :resource
-
-      # Configure default email options
-      def devise_mail(record, action, opts = {}, &block)
-        initialize_from_record(record)
-        mail headers_for(action, opts), &block
-      end
-
-      def initialize_from_record(record)
-        @scope_name = Devise::Mapping.find_scope!(record)
-        @resource   = instance_variable_set("@#{devise_mapping.name}", record)
-      end
-
-      def devise_mapping
-        @devise_mapping ||= Devise.mappings[scope_name]
-      end
-
-      def headers_for(action, opts)
-        headers = {
-          subject: subject_for(action),
-          to: resource.email,
-          from: mailer_sender(devise_mapping),
-          reply_to: mailer_reply_to(devise_mapping),
-          template_path: template_paths,
-          template_name: action
-        }.merge(opts)
-
-        @email = headers[:to]
-        headers
-      end
-
-      def mailer_reply_to(mapping)
-        mailer_sender(mapping, :reply_to)
-      end
-
-      def mailer_from(mapping)
-        mailer_sender(mapping, :from)
-      end
-
-      def mailer_sender(mapping, sender = :from)
-        default_sender = default_params[sender]
-        if default_sender.present?
-          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
-        elsif Devise.mailer_sender.is_a?(Proc)
-          Devise.mailer_sender.call(mapping.name)
-        else
-          Devise.mailer_sender
-        end
+        layout 'mailer' rescue true
       end
 
       def template_paths
         template_path = _prefixes.dup
         template_path.unshift "#{@devise_mapping.scoped_path}/mailer" if self.class.scoped_views?
         template_path
+      end
+
+      def layout_path
+        'mailer'
       end
 
       # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
